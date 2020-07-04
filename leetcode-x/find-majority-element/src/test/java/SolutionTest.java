3c333606dfb423c5ecadf0dcec894674b2f95f9b
Randy-xuefm <fenming.xue@outlook.com>
2020-07-04 17:07:30 +0800

    leetcode -- find majority element

--- leetcode-x/find-majority-element/src/test/java/SolutionTest.java
+++ leetcode-x/find-majority-element/src/test/java/SolutionTest.java
@@ -17,4 +17,16 @@ class SolutionTest {
         int[] nums= {1,2};
         Assertions.assertEquals(-1,Solution.majorityElement(nums));
     }
+
+    @Test
+    void majorityElement4moer() {
+        int[] nums= {1,2,2,2,2,2,3,3,1};
+        Assertions.assertEquals(2,Solution.majorityElement4moer(nums));
+    }
+
+    @Test
+    void majorityElement4moer1() {
+        int[] nums= {1,2};
+        Assertions.assertEquals(-1,Solution.majorityElement4moer(nums));
+    }
 }
